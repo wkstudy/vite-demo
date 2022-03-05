8921be12fc0f436114f8bc71cb2f9f915b44c1f5
wangkai53 <wangkai53@sfmail.sf-express.com>
2022-03-06 01:45:45 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="hello"></div>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.hello {
+  height: 300px;
+  background-image: url(./assets/logo.png);
+}
+</style>
